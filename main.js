294e6512250e52d2d84f4dde0468db2f76db2386
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-09-11 21:55:23 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+///////////////// JAVASCRIPT BASICS
+//////////// VAR
+// var message = 'Hello!';
+// console.log(message);
+// message = "Hello from JavaScript Basics"
+// console.log(message)
+
+// EX:
+
+// var myName = "Ryan";
