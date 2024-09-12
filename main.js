b5407d6ddd10cd3fc53f065352f2e852d3b43c9c
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-09-11 21:58:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -90,3 +90,46 @@
 // let role = 'developer';
 
 // let msg = firstName + " " + lastName + " : " + role.toUpperCase()
+
+
+
+
+//////////// TEMPLATE LITERALS
+// const name = prompt('What is your name?');
+
+// const message = `Hello ${name}. fWelcome to my music site. I'm happy you came back to visit, ${name}. Please come back again and listen to more music!`;
+
+// console.log(message);
+
+
+
+
+//////////// SHOUT .JS
+// const stringToShout = prompt("What do you want to shout?");
+// const shout = stringToShout.toLocaleUpperCase();
+// const shoutMessage = `The message to shout is: ${shout}!!!!`
+// console.log(shoutMessage)
+
+
+// const stringToShout = prompt("What do you want to shout?");
+// const shout = stringToShout.toLocaleUpperCase();
+// const shoutMessage = `<h2>The message to shout is: ${shout}!!!!</h2>`
+// document.querySelector('main').innerHTML = shoutMessage
+
+// EX:
+// 1. Declare variables and capture input.
+// let noun = prompt("Give me a noun");
+// let verb = prompt("Give me a verb");
+
+// // 2. Combine the input with other words to create a story.
+// let inputVerbAndNoun = `I like to ${noun} while doing the ${verb} all the time and it makes me happy in my pants!!`;
+
+// // 3. Display the story as a <p> inside the <main> element.
+// let madLibStory = `<p>this madlib is about ${inputVerbAndNoun}</p>`;
+// document.querySelector('main').innerHTML = madLibStory
+
+// const flavor = "Blueberry";
+// const type = "Smoothie";
+// const price = 4.99;
+
+// const drink = `${flavor} ${type}: $${price}`;
